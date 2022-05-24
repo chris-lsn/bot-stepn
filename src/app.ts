cd48825b4575102de4e8edab5647f0ec63eecc8c
Chris Leung <me@chrisleung.me>
2022-05-25 01:04:10 +0800

    Add express web server

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,19 @@ import { Config, Platform } from './config'
 import { Slack } from './platforms/slack';
 import { BasePlatform } from './platforms/platform';
 import { Telegram } from './platforms/telegram';
-
+const app = require('express')()
 const config: Config = Config.getInstance();
 
-(async () => {
-  let bot: BasePlatform
-  switch (config.platform) {
-    
-    case Platform.SLACK:
-      bot = new Slack(config.slackToken, config.slackSecret)
-      break;
-    case Platform.TELEGRAM:
-      bot = new Telegram(config.telegramToken)
-      break;
-  }
-  bot.listen()
-  console.log('⚡️ STEPN BOT is running!')
-})()
+let bot: BasePlatform
+switch (config.platform) {
+  case Platform.SLACK:
+    bot = new Slack(config.slackToken, config.slackSecret)
+    break;
+  case Platform.TELEGRAM:
+    bot = new Telegram(config.telegramToken)
+    break;
+}
+bot.listen()
+console.log('⚡️ STEPN BOT is running!')
+app.listen(config.port)
+
